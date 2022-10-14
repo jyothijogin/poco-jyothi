45dd06ac4fe1557b109329cad2c819dde3f8197b
jyothijogin <jyothijogin11@gmail.com>
2022-10-14 16:32:39 +0200

    Made it more dynamic by removing image tags from html and used map function

--- project-brief/myScript.js
+++ project-brief/myScript.js
@@ -3,6 +3,7 @@ let persons = [
     id: "0",
     name: "Muhammad Bilal Sulaimankhil",
     country: "Afghanistan",
+    gender: "male",
     skills: "HTML, CSS, FIGMA, MS OFFICE, JAVASCRIPT, GITHUB, Bootstrap, SCSS",
     url: "./images/00-Powercoders_Portrait_2.jpg",
   },
@@ -11,6 +12,7 @@ let persons = [
     id: "1",
     name: "Ibrahim Halil Kuray",
     country: "Turkey",
+    gender: "male",
     skills:
       "HTML, CSS, JAVASCRIPT, Python, Typescript, Sass, React JS, Hooks and Context API, Node JS,NPM, MySQL, Express JS, Rest API, Bootstrap,Figma, CLI, Git and Github, Atlassian-Jira & Scrum, Agile",
     url: "./images/01-Powercoders_Portrait_51.jpg",
@@ -20,6 +22,7 @@ let persons = [
     id: "2",
     name: "Ismet Bakay",
     country: "Turkey",
+    gender: "male",
     skills:
       "Javascript, React, Angular, Node.js,Express.Js Rest APIs, MySQL, MongoDB, Figma",
     url: "./images/02-Powercoders_Portrait_6.jpg",
@@ -29,6 +32,7 @@ let persons = [
     id: "3",
     name: "Mustafa Cihan",
     country: "Turkey",
+    gender: "male",
     skills:
       "HTML, CSS, BOOTSTRAP, RESPONSIVE DESIGN, JAVASCRIPT, REACT, GIT/GITHUB",
     url: "./images/04-Powercoders_Portrait_54.jpg",
@@ -38,6 +42,7 @@ let persons = [
     id: "4",
     name: "Abdul Fatah Shieryar",
     country: "Afghanistan",
+    gender: "male",
     skills:
       "MICROSOFT WINDOWS, MICROSOFT OFFICE, HARDWARE, HTML, CSS, NETWORKING, Java",
     url: "./images/04-Powercoders_Portrait_8.jpg",
@@ -47,6 +52,7 @@ let persons = [
     id: "5",
     name: "Yuliia Kuts",
     country: "Ukraine",
+    gender: "female",
     skills: "HTML, CSS, JAVA, MYSQL, PYTHON",
     url: "./images/06-Powercoders_Portrait_10.jpg",
   },
@@ -55,6 +61,7 @@ let persons = [
     id: "6",
     name: "Kadriye Tuba Turkcan Karaca",
     country: "Turkey",
+    gender: "female",
     skills:
       "PYTHON, PANDAS, NUMPY, SCIKIT LEARN, EDA, DATA VISUALIZATION, REGRESSION, CLUSTERING, CLASSIFICATION",
     url: "./images/08-Powercoders_Portrait_12.jpg",
@@ -64,6 +71,7 @@ let persons = [
     id: "7",
     name: "Hasan Danaci",
     country: "Turkey",
+    gender: "male",
     skills: "HTML, CSS, JAVASCRIPT",
     url: "./images/09-Powercoders_Portrait_13.jpg",
   },
@@ -72,6 +80,7 @@ let persons = [
     id: "8",
     name: "Ni Luh Ariani",
     country: "Indonesia",
+    gender: "female",
     skills: "HTML, CSS, MS OFFICE, ECOMMERCE, JAVASCRIPT",
     url: "./images/26-Powercoders_Portrait_32.jpg",
   },
@@ -80,6 +89,7 @@ let persons = [
     id: "9",
     name: "Olena Smetiuk",
     country: "Ukraine",
+    gender: "female",
     skills: "HTML5, CSS3, BEM, Git, JAVASCRIPT, MS OFFICE, CLI",
     url: "./images/24-Powercoders_Portrait_30.jpg",
   },
@@ -88,6 +98,7 @@ let persons = [
     id: "10",
     name: "Oguz Yildiz",
     country: "Turkey",
+    gender: "male",
     skills:
       "HTML, CSS, SASS, Bootstrap, Tailwind, JavaScript, TypeScript, React Django, Flask, ASP.NET, Node.js, Express.js, SQL, MongoDB, C#, Python, Pandas, Numpy, Matplotlib, Docker, Kubernetes, OpenShift, TDD, CI/CD, Tekton, IBM Cloud, AWS, Agile Methodologies, Scrum, (Familiar With): C++, Java",
     url: "./images/14-Powercoders_Portrait_18.jpg",
@@ -97,6 +108,7 @@ let persons = [
     id: "11",
     name: "Yusuf Ilhan",
     country: "Turkey",
+    gender: "male",
     skills:
       "SPLUNK, IBM QRADAR, SENTINELONE, CROWDSTRIKE, HTML, CSS, JAVASCRIPT, PYTHON",
     url: "./images/16-Powercoders_Portrait_21.jpg",
@@ -106,6 +118,7 @@ let persons = [
     id: "12",
     name: "Emin Samed Yilmaz",
     country: "Turkey",
+    gender: "male",
     skills:
       "Html, CSS, Javascript, Responsive Design, Bootstrap, React, MySQL, Postman, NodeJS, Express.Js, Rest API, Figma, Git & Github, Python, Angular, Aws Cloud",
     url: "./images/18-Powercoders_Portrait_23.jpg",
@@ -115,114 +128,123 @@ let persons = [
     id: "13",
     name: "Enes Bora",
     country: "Turkey",
+    gender: "male",
     skills:
       "CYBERSECURITY, SPLUNK, IBM QRADAR, CROWDSTRIKE, IDS, IPS, KALI LINUX, WIRESHARK, JAVASCRIPT, HTML, CSS, MICROSOFT AZURE, WEB APPLICATION SECURITY, REACT, SQL, PYTHON, R",
-      url: "./images/20-Powercoders_Portrait_26.jpg"
+    url: "./images/20-Powercoders_Portrait_26.jpg",
   },
 
   {
     id: "14",
     name: "Jyothi",
     country: "India",
+    gender: "female",
     skills:
       "HTML, CSS, JAVASCRIPT, JAVA, JAVA EE, SPRING BOOT, JPA, HIBERNATE, GITHUB, GIT, MAVEN, ANGULAR",
-      url: "./images/23-Powercoders_Portrait_29.jpg"
+    url: "./images/23-Powercoders_Portrait_29.jpg",
   },
 
   {
     id: "15",
     name: "Shamil Idrisov",
     country: "Switzerland",
+    gender: "male",
     skills: "HTML, CSS, Django, JAVA SE, Python, Kotlin, SQL, Vue.JS, JS",
-    url: "./images/30-Powercoders_Portrait_36.jpg"
+    url: "./images/30-Powercoders_Portrait_36.jpg",
   },
 
   {
     id: "16",
     name: "Fatih Yavuz",
     country: "Turkey",
+    gender: "male",
     skills:
       "Excel, VHDL, HTML, CSS, Javascript, SQL, Python, Node.js, MatLab, Java, C#",
-      url: "./images/31-Powercoders_Portrait_37.jpg"
+    url: "./images/31-Powercoders_Portrait_37.jpg",
   },
 
   {
     id: "17",
     name: "Baris Durucan",
     country: "Turkey",
+    gender: "male",
     skills: "HTML, CSS, JAVA, CYBER SECURITY, GOOGLE SHEETS, DATA ANALYTICS",
-    url: "./images/32-Powercoders_Portrait_38.jpg"
+    url: "./images/32-Powercoders_Portrait_38.jpg",
   },
 
   {
     id: "18",
     name: "Sevim Gokturk",
     country: "Turkey",
+    gender: "female",
     skills:
       "JAVA, MYSQL, SELENIUM, CUCUMBER, API REST ASSURED, TestNG, POSTMAN, GIT/GITHUB, SDLC, HTML, CSS, JAVA SCRIPT, JIRA, JDBC, JENKINS",
-      url: "./images/34-Powercoders_Portrait_40.jpg"
+    url: "./images/34-Powercoders_Portrait_40.jpg",
   },
 
   {
     id: "19",
     name: "Murat Guldibi",
     country: "Turkey",
+    gender: "male",
     skills:
       "HTML, CSS, Javascript, MySQL, Sequelize(ORM), ReactJS, NodeJS, ExpressJS, Material UI, Bootstrap, Git, GitHub, Rest API, Postman, Agile, JAVA, OOP, Auth0",
-      url: "./images/33-Powercoders_Portrait_39.jpg"
+    url: "./images/33-Powercoders_Portrait_39.jpg",
   },
 
   {
     id: "20",
     name: "Senay Atli",
     country: "Turkey",
+    gender: "female",
     skills:
       "HTML, CSS, MSOFFICE, MSSQL, BOOTSTRAP ,JAVASCRIPT, PYTHON, COMPUTER HARDWARE, C++",
-      url: "./images/35-Powercoders_Portrait_41.jpg"
+    url: "./images/35-Powercoders_Portrait_41.jpg",
   },
 
   {
     id: "21",
     name: "Arwa Alhussein",
     country: "Syria",
+    gender: "male",
     skills: "Microsoft OFFICE, SAP, ABACUS, WinBIZ, HTML, CSS, JS, GIT, React",
-    url: "./images/37-Powercoders_Portrait_43.jpg"
+    url: "./images/37-Powercoders_Portrait_43.jpg",
   },
 
   {
     id: "22",
     name: "Sadettin Karatas",
     country: "Turkey",
+    gender: "male",
     skills: "HTML5, CSS3, JAVASCRIPT, MYSQL,MSOFFICE, PYTHON, JAVA",
-    url: "./images/28-Powercoders_Portrait_34.jpg"
+    url: "./images/28-Powercoders_Portrait_34.jpg",
   },
 
   {
     id: "23",
     name: "Ali Göcmen",
     country: "Turkey",
+    gender: "male",
     skills:
       "C#, Unity, Xamarin, SQL, PL-SQL, Oracle Database, Java, Python, Google Cloud Services(Firebase,Firestore), HTML 5, CSS 3, Javascript, Google IAP, AD, Analyzing libraries, Google, Unity, VS AppCenter), Google Cloud Services (Storage, Functions, Analyze, BigQuery), Microsoft Power BI, IBM Cloud & Watson",
-      url: "./images/41-Powercoders_Portrait_47.jpg"
+    url: "./images/41-Powercoders_Portrait_47.jpg",
   },
 ];
 
 //display the details when you click the image
 
- let container = document.getElementById('container');
+let container = document.getElementById("container");
 
- 
-
-/* container.innerHTML = persons.map(person => 
-    {
-    return (`  
-    <div class="picture male" id="${person.id}">
+container.innerHTML = persons
+  .map((person) => {
+    return `  
+    <div class="picture male">
         <img src="${person.url}" alt="Photo of ${person.name}" onClick="displayInfo(${person.id})">
         <div>${person.name}</div>
     </div>
-    `)
-    
-  }).join(' ') */
+    `;
+  })
+  .join(" ");
 
 function displayInfo(id) {
   if (id === "") {
@@ -244,38 +266,49 @@ function displayInfo(id) {
 // //filter by gender code
 
 function filterBy(gender) {
-  if (gender === "f") {
-    // hide all male by adding the hide class
-    const maleProfiles = document.querySelectorAll(".male");
-    maleProfiles.forEach((node) => {
-      node.classList.add("hide");
-    });
-    // make sure hide class is removed from all females
-    const femaleProfiles = document.querySelectorAll(".female");
-    femaleProfiles.forEach((node) => {
-      console.log(node.classList);
-      node.classList.remove("hide");
-    });
-  } else if (gender === "m") {
-    const femaleProfiles = document.querySelectorAll(".female");
-    femaleProfiles.forEach((node) => {
-      node.classList.add("hide");
-    });
 
-    const maleProfiles = document.querySelectorAll(".male");
-    maleProfiles.forEach((node) => {
-      node.classList.remove("hide");
+  if (gender === "m") {
+    const males = persons.filter(function (p) {
+      return p.gender === "male";
     });
+    container.innerHTML = males
+      .map((person) => {
+        return `  
+    <div class="picture">
+        <img src="${person.url}" alt="Photo of ${person.name}" onClick="displayInfo(${person.id})">
+        <div>${person.name}</div>
+    </div>
+    `;
+      })
+      .join(" ");
+    
+  } else if (gender === "f") {
+    const females = persons.filter(function (p) {
+      return p.gender === "female";
+    });
+    container.innerHTML = females
+      .map((person) => {
+        return `  
+        <div class="picture">
+            <img src="${person.url}" alt="Photo of ${person.name}" onClick="displayInfo(${person.id})">
+            <div>${person.name}</div>
+        </div>
+        `;
+      })
+      .join(" ");
+    
   } else {
-    const femaleProfiles = document.querySelectorAll(".female");
-    femaleProfiles.forEach((node) => {
-      console.log(node.classList);
-      node.classList.remove("hide");
-    });
-
-    const maleProfiles = document.querySelectorAll(".male");
-    maleProfiles.forEach((node) => {
-      node.classList.remove("hide");
-    });
+    container.innerHTML = persons
+      .map((person) => {
+        return `  
+                <div class="picture male">
+                    <img src="${person.url}" alt="Photo of ${person.name}" onClick="displayInfo(${person.id})">
+                    <div>${person.name}</div>
+                </div>
+                `;
+      })
+      .join(" ");
   }
+
+  
 }
