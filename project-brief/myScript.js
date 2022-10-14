ac0f60dcd3b06ed464389e688c42301ecf4689fe
jyothijogin <jyothijogin11@gmail.com>
2022-10-14 15:40:58 +0200

    This is my first commit

---
+++ project-brief/myScript.js
@@ -0,0 +1,281 @@
+let persons = [
+  {
+    id: "0",
+    name: "Muhammad Bilal Sulaimankhil",
+    country: "Afghanistan",
+    skills: "HTML, CSS, FIGMA, MS OFFICE, JAVASCRIPT, GITHUB, Bootstrap, SCSS",
+    url: "./images/00-Powercoders_Portrait_2.jpg",
+  },
+
+  {
+    id: "1",
+    name: "Ibrahim Halil Kuray",
+    country: "Turkey",
+    skills:
+      "HTML, CSS, JAVASCRIPT, Python, Typescript, Sass, React JS, Hooks and Context API, Node JS,NPM, MySQL, Express JS, Rest API, Bootstrap,Figma, CLI, Git and Github, Atlassian-Jira & Scrum, Agile",
+    url: "./images/01-Powercoders_Portrait_51.jpg",
+  },
+
+  {
+    id: "2",
+    name: "Ismet Bakay",
+    country: "Turkey",
+    skills:
+      "Javascript, React, Angular, Node.js,Express.Js Rest APIs, MySQL, MongoDB, Figma",
+    url: "./images/02-Powercoders_Portrait_6.jpg",
+  },
+
+  {
+    id: "3",
+    name: "Mustafa Cihan",
+    country: "Turkey",
+    skills:
+      "HTML, CSS, BOOTSTRAP, RESPONSIVE DESIGN, JAVASCRIPT, REACT, GIT/GITHUB",
+    url: "./images/04-Powercoders_Portrait_54.jpg",
+  },
+
+  {
+    id: "4",
+    name: "Abdul Fatah Shieryar",
+    country: "Afghanistan",
+    skills:
+      "MICROSOFT WINDOWS, MICROSOFT OFFICE, HARDWARE, HTML, CSS, NETWORKING, Java",
+    url: "./images/04-Powercoders_Portrait_8.jpg",
+  },
+
+  {
+    id: "5",
+    name: "Yuliia Kuts",
+    country: "Ukraine",
+    skills: "HTML, CSS, JAVA, MYSQL, PYTHON",
+    url: "./images/06-Powercoders_Portrait_10.jpg",
+  },
+
+  {
+    id: "6",
+    name: "Kadriye Tuba Turkcan Karaca",
+    country: "Turkey",
+    skills:
+      "PYTHON, PANDAS, NUMPY, SCIKIT LEARN, EDA, DATA VISUALIZATION, REGRESSION, CLUSTERING, CLASSIFICATION",
+    url: "./images/08-Powercoders_Portrait_12.jpg",
+  },
+
+  {
+    id: "7",
+    name: "Hasan Danaci",
+    country: "Turkey",
+    skills: "HTML, CSS, JAVASCRIPT",
+    url: "./images/09-Powercoders_Portrait_13.jpg",
+  },
+
+  {
+    id: "8",
+    name: "Ni Luh Ariani",
+    country: "Indonesia",
+    skills: "HTML, CSS, MS OFFICE, ECOMMERCE, JAVASCRIPT",
+    url: "./images/26-Powercoders_Portrait_32.jpg",
+  },
+
+  {
+    id: "9",
+    name: "Olena Smetiuk",
+    country: "Ukraine",
+    skills: "HTML5, CSS3, BEM, Git, JAVASCRIPT, MS OFFICE, CLI",
+    url: "./images/24-Powercoders_Portrait_30.jpg",
+  },
+
+  {
+    id: "10",
+    name: "Oguz Yildiz",
+    country: "Turkey",
+    skills:
+      "HTML, CSS, SASS, Bootstrap, Tailwind, JavaScript, TypeScript, React Django, Flask, ASP.NET, Node.js, Express.js, SQL, MongoDB, C#, Python, Pandas, Numpy, Matplotlib, Docker, Kubernetes, OpenShift, TDD, CI/CD, Tekton, IBM Cloud, AWS, Agile Methodologies, Scrum, (Familiar With): C++, Java",
+    url: "./images/14-Powercoders_Portrait_18.jpg",
+  },
+
+  {
+    id: "11",
+    name: "Yusuf Ilhan",
+    country: "Turkey",
+    skills:
+      "SPLUNK, IBM QRADAR, SENTINELONE, CROWDSTRIKE, HTML, CSS, JAVASCRIPT, PYTHON",
+    url: "./images/16-Powercoders_Portrait_21.jpg",
+  },
+
+  {
+    id: "12",
+    name: "Emin Samed Yilmaz",
+    country: "Turkey",
+    skills:
+      "Html, CSS, Javascript, Responsive Design, Bootstrap, React, MySQL, Postman, NodeJS, Express.Js, Rest API, Figma, Git & Github, Python, Angular, Aws Cloud",
+    url: "./images/18-Powercoders_Portrait_23.jpg",
+  },
+
+  {
+    id: "13",
+    name: "Enes Bora",
+    country: "Turkey",
+    skills:
+      "CYBERSECURITY, SPLUNK, IBM QRADAR, CROWDSTRIKE, IDS, IPS, KALI LINUX, WIRESHARK, JAVASCRIPT, HTML, CSS, MICROSOFT AZURE, WEB APPLICATION SECURITY, REACT, SQL, PYTHON, R",
+      url: "./images/20-Powercoders_Portrait_26.jpg"
+  },
+
+  {
+    id: "14",
+    name: "Jyothi",
+    country: "India",
+    skills:
+      "HTML, CSS, JAVASCRIPT, JAVA, JAVA EE, SPRING BOOT, JPA, HIBERNATE, GITHUB, GIT, MAVEN, ANGULAR",
+      url: "./images/23-Powercoders_Portrait_29.jpg"
+  },
+
+  {
+    id: "15",
+    name: "Shamil Idrisov",
+    country: "Switzerland",
+    skills: "HTML, CSS, Django, JAVA SE, Python, Kotlin, SQL, Vue.JS, JS",
+    url: "./images/30-Powercoders_Portrait_36.jpg"
+  },
+
+  {
+    id: "16",
+    name: "Fatih Yavuz",
+    country: "Turkey",
+    skills:
+      "Excel, VHDL, HTML, CSS, Javascript, SQL, Python, Node.js, MatLab, Java, C#",
+      url: "./images/31-Powercoders_Portrait_37.jpg"
+  },
+
+  {
+    id: "17",
+    name: "Baris Durucan",
+    country: "Turkey",
+    skills: "HTML, CSS, JAVA, CYBER SECURITY, GOOGLE SHEETS, DATA ANALYTICS",
+    url: "./images/32-Powercoders_Portrait_38.jpg"
+  },
+
+  {
+    id: "18",
+    name: "Sevim Gokturk",
+    country: "Turkey",
+    skills:
+      "JAVA, MYSQL, SELENIUM, CUCUMBER, API REST ASSURED, TestNG, POSTMAN, GIT/GITHUB, SDLC, HTML, CSS, JAVA SCRIPT, JIRA, JDBC, JENKINS",
+      url: "./images/34-Powercoders_Portrait_40.jpg"
+  },
+
+  {
+    id: "19",
+    name: "Murat Guldibi",
+    country: "Turkey",
+    skills:
+      "HTML, CSS, Javascript, MySQL, Sequelize(ORM), ReactJS, NodeJS, ExpressJS, Material UI, Bootstrap, Git, GitHub, Rest API, Postman, Agile, JAVA, OOP, Auth0",
+      url: "./images/33-Powercoders_Portrait_39.jpg"
+  },
+
+  {
+    id: "20",
+    name: "Senay Atli",
+    country: "Turkey",
+    skills:
+      "HTML, CSS, MSOFFICE, MSSQL, BOOTSTRAP ,JAVASCRIPT, PYTHON, COMPUTER HARDWARE, C++",
+      url: "./images/35-Powercoders_Portrait_41.jpg"
+  },
+
+  {
+    id: "21",
+    name: "Arwa Alhussein",
+    country: "Syria",
+    skills: "Microsoft OFFICE, SAP, ABACUS, WinBIZ, HTML, CSS, JS, GIT, React",
+    url: "./images/37-Powercoders_Portrait_43.jpg"
+  },
+
+  {
+    id: "22",
+    name: "Sadettin Karatas",
+    country: "Turkey",
+    skills: "HTML5, CSS3, JAVASCRIPT, MYSQL,MSOFFICE, PYTHON, JAVA",
+    url: "./images/28-Powercoders_Portrait_34.jpg"
+  },
+
+  {
+    id: "23",
+    name: "Ali Göcmen",
+    country: "Turkey",
+    skills:
+      "C#, Unity, Xamarin, SQL, PL-SQL, Oracle Database, Java, Python, Google Cloud Services(Firebase,Firestore), HTML 5, CSS 3, Javascript, Google IAP, AD, Analyzing libraries, Google, Unity, VS AppCenter), Google Cloud Services (Storage, Functions, Analyze, BigQuery), Microsoft Power BI, IBM Cloud & Watson",
+      url: "./images/41-Powercoders_Portrait_47.jpg"
+  },
+];
+
+//display the details when you click the image
+
+ let container = document.getElementById('container');
+
+ 
+
+/* container.innerHTML = persons.map(person => 
+    {
+    return (`  
+    <div class="picture male" id="${person.id}">
+        <img src="${person.url}" alt="Photo of ${person.name}" onClick="displayInfo(${person.id})">
+        <div>${person.name}</div>
+    </div>
+    `)
+    
+  }).join(' ') */
+
+function displayInfo(id) {
+  if (id === "") {
+    document.getElementById("popup-1").classList.toggle("active");
+  } else {
+    document.getElementById("popup-1").classList.toggle("active");
+    document.getElementById("popup-1").style.display = "block";
+
+    let header = document.getElementById("head-tag");
+    let country = document.getElementById("country");
+    let skills = document.getElementById("skills");
+    header.innerText = persons[id].name;
+
+    country.innerText = "COUNTRY - " + persons[id].country;
+    skills.innerText = "SKILLS - " + persons[id].skills;
+  }
+}
+
+// //filter by gender code
+
+function filterBy(gender) {
+  if (gender === "f") {
+    // hide all male by adding the hide class
+    const maleProfiles = document.querySelectorAll(".male");
+    maleProfiles.forEach((node) => {
+      node.classList.add("hide");
+    });
+    // make sure hide class is removed from all females
+    const femaleProfiles = document.querySelectorAll(".female");
+    femaleProfiles.forEach((node) => {
+      console.log(node.classList);
+      node.classList.remove("hide");
+    });
+  } else if (gender === "m") {
+    const femaleProfiles = document.querySelectorAll(".female");
+    femaleProfiles.forEach((node) => {
+      node.classList.add("hide");
+    });
+
+    const maleProfiles = document.querySelectorAll(".male");
+    maleProfiles.forEach((node) => {
+      node.classList.remove("hide");
+    });
+  } else {
+    const femaleProfiles = document.querySelectorAll(".female");
+    femaleProfiles.forEach((node) => {
+      console.log(node.classList);
+      node.classList.remove("hide");
+    });
+
+    const maleProfiles = document.querySelectorAll(".male");
+    maleProfiles.forEach((node) => {
+      node.classList.remove("hide");
+    });
+  }
+}
