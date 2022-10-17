cc4b9e0507afe8440d4a19afbd9dcd5cdd512f9f
jyothijogin <jyothijogin11@gmail.com>
2022-10-17 15:11:56 +0200

    Removed unwanted code

--- odd-even/myJavascript.js
+++ odd-even/myJavascript.js
@@ -1,4 +1,4 @@
-/* const input = prompt("Enter a number between 0 and 20");
+ const input = prompt("Enter a number between 0 and 20");
 
 if(input < 0 || input > 20) {
     window.alert("Incorrect value");
@@ -9,22 +9,4 @@ else if (input % 2 === 0 ) {
 }
 else {
     window.alert(input + " is odd");
-} */
-
-let mysteryNumber = getRandomArbitrary(0,30);
-let input;
-while((input = prompt("Guess the mystery number between 0 and 30")) != mysteryNumber) {
-    if (input < mysteryNumber) {
-        window.alert("Try a bigger number");
-    }
-    else if(input > mysteryNumber) {
-        window.alert("Try a smaller number");
-    }
-
-}
-
-window.alert("You found it!! " + mysteryNumber);
-
-function getRandomArbitrary(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
+} 
