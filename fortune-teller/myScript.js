6aa6bbd7e823ef3a4be4121dd76d1d41a62206a5
jyothijogin <jyothijogin11@gmail.com>
2022-10-17 13:40:35 +0200

    This is my first commit

---
+++ fortune-teller/myScript.js
@@ -0,0 +1,9 @@
+
+
+const geoLocation = prompt("Enter your location");
+const partnerName = prompt("Enter your partner's name");
+const noOfKids = prompt("Enter the number of kids");
+const jobTitle = prompt("Enter your job title");
+
+window.alert("\"You will be a " + jobTitle + " in " + geoLocation +", and married to " + partnerName +
+              " with " + noOfKids + " kids.\"");
