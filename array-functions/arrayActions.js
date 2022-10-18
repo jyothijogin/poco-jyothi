85fb77f938ec55642c1dfa7143ce0e18023d48d8
jyothijogin <jyothijogin11@gmail.com>
2022-10-18 14:31:19 +0200

    This is my first commit

---
+++ array-functions/arrayActions.js
@@ -0,0 +1,21 @@
+//1. Access and output Orange
+var array = ["Banana", "Apple", "Orange", "Blueberry"];
+console.log(array[2]);
+
+//2. Remove the Banana from the array
+array.shift();
+console.log(array);
+
+//3. Sort the array in order
+console.log(array.sort());
+
+//4. Put Kiwi at the end of the array
+array.push("Kiwi");
+console.log(array);
+
+//5. Remove Apple from the array
+array.shift();
+console.log(array);
+
+//6. Reverse the order of the array
+console.log(array.reverse());
