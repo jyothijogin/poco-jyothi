967b41b969a63959145402efd3c084a5663eab44
jyothijogin <jyothijogin11@gmail.com>
2022-10-18 14:29:26 +0200

    This is my first commit

---
+++ recipe-card/script.js
@@ -0,0 +1,10 @@
+const recipe = {
+    title: "Milk Rice",
+    servings: "6",
+    ingredients: ["1 cup Rice", "1 glass Milk", "2 Tbsp Sugar", "1 Tbsp Butter"]
+};
+
+
+console.log("Title - " + recipe.title);
+console.log("Servings - " +recipe.servings);
+console.log("Ingredients - " + recipe.ingredients);
