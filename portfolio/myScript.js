9885843bd2b0280a108901da3be602cb068aa8f3
jyothijogin <jyothijogin11@gmail.com>
2022-10-31 22:10:11 +0100

    Added hobbies section

--- portfolio/myScript.js
+++ portfolio/myScript.js
@@ -29,9 +29,7 @@ function closeMenu() {
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
+window.onscroll = function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
